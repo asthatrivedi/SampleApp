827677c9925a2573a7990de41aa1c6f112e37586
Astha <astha@Asthas-MacBook-Pro.local>
2014-03-01 12:30:28 -0800

    Initial Commit

---
+++ SampleApp/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  SampleApp
+//
+//  Created by Astha on 3/1/14.
+//  Copyright (c) 2014 Astha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Astha on 3/1/14.
+//  Copyright (c) 2014 Astha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+    }
+}
